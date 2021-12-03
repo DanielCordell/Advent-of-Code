2163718ce434a582f18a2339e35b4d941b4cb57f
Daniel <danielbc@hotmail.co.uk>
2021-12-03 18:42:03 +0000

    Emptying day 3

--- 2021/src/Day3/part1.kt
+++ 2021/src/Day3/part1.kt
@@ -1,19 +1,8 @@
-package Day2
+package Day3
 
-import Shared.fileAsSplitStrings
+import Shared.fileAsInts
 
 fun main()
 {
-    var input = fileAsSplitStrings("src/Day2/input.txt").map { it[0] to it[1].toInt() }
-    var output = input.fold(0 to 0) { curr, inst ->
-        when(inst.first)
-        {
-            "forward" -> curr.first + inst.second to curr.second
-            "up" -> curr.first to curr.second - inst.second
-            "down" -> curr.first to curr.second + inst.second
-            else -> curr
-        }
-    }
-
-    println(output.first * output.second)
+    var input = fileAsInts("src/Day3/input.txt")
 }
--- 2021/src/Day3/part2.kt
+++ 2021/src/Day3/part2.kt
@@ -1,19 +1,9 @@
-package Day2
+package Day3
 
-import Shared.fileAsSplitStrings
+import Shared.fileAsInts
 
 fun main()
 {
-    var input = fileAsSplitStrings("src/Day2/input.txt").map { it[0] to it[1].toInt() }
-    var output = input.fold(Triple(0, 0, 0)) { curr, inst ->
-        when(inst.first)
-        {
-            "forward" -> Triple(curr.first + inst.second, curr.second + inst.second * curr.third, curr.third)
-            "up" -> Triple(curr.first, curr.second, curr.third - inst.second)
-            "down" -> Triple(curr.first, curr.second, curr.third + inst.second)
-            else -> curr
-        }
-    }
+    var input = fileAsInts("src/Day3/input.txt")
 
-    println(output.first * output.second)
 }
