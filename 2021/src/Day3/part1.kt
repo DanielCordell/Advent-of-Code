4eb85a2bd3f970cae4037baa81377c4f80ce02e0
Daniel <danielbc@hotmail.co.uk>
2021-12-03 19:07:40 +0000

    Day 3 part 1

--- 2021/src/Day3/part1.kt
+++ 2021/src/Day3/part1.kt
@@ -1,8 +1,17 @@
 package Day3
 
-import Shared.fileAsInts
+import Shared.fileAsStrings
 
 fun main()
 {
-    var input = fileAsInts("src/Day3/input.txt")
+    var input = fileAsStrings("src/Day3/input.txt")
+
+    var output = input.fold(List(input[0].length) {0 to 0}) { columns, row ->
+        row.mapIndexed { i, c -> (if (c == '0') (columns[i].first + 1) to columns[i].second else columns[i].first to (columns[i].second + 1)) }
+    }.fold("" to "") {acc, bitcount -> when {
+        bitcount.first > bitcount.second -> acc.first + "0" to acc.second + "1"
+        else -> acc.first + "1" to acc.second + "0"
+    } }.let { Integer.parseInt(it.first, 2) to Integer.parseInt(it.second, 2) }
+
+    println(output.first * output.second)
 }
--- 2021/src/Shared/FileReader.kt
+++ 2021/src/Shared/FileReader.kt
@@ -5,5 +5,9 @@ import java.io.File
 fun fileAsInts(fileName: String): List<Int>
         = File(fileName).useLines { it.map { line -> line.toInt() }.toList() }
 
+fun fileAsStrings(fileName: String): List<String>
+        = File(fileName).readLines()
+
+
 fun fileAsSplitStrings(fileName: String) : List<List<String>>
         = File(fileName).useLines { it.map { line -> line.split(" ") }.toList() }
